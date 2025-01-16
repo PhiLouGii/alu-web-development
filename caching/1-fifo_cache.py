3129bc2ac02f05bc210b8132102fe0bb476aa4a8
Philippa Louise Giibwa <125802100+PhiLouGii@users.noreply.github.com>
2025-01-16 16:16:17 +0300

    done

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -1,43 +1,50 @@
-#!/usr/bin/python3
-""" FIFO caching """
+#!/usr/bin/env python3
+"""
+FIFO Cache Module
+"""
+
+
 from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """ Class that inherits from BaseCaching and is a caching system """
+    """
+    FIFO caching system where the first item added
+    is the first one to be discarded
+    once the cache reaches its limit.
+    """
 
     def __init__(self):
+        """
+        Initialize the cache system and keep track
+        of the order of items.
+        """
         super().__init__()
-        self.data = {}
-        self.next_in, self.next_out = 0, 0
-
-    def _pop(self):
-        """ FIFO algorithm, remove element """
-        self.next_out += 1
-        key = self.data[self.next_out]
-        del self.data[self.next_out], self.cache_data[key]
-
-    def _push(self, key, item):
-        """ FIFO algorithm, add element """
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS - 1:
-            print("DISCARD: {}".format(self.data[self.next_out + 1]))
-            self.pop()
-        self.cache_data[key] = item
-        self.next_in += 1
-        self.data[self.next_in] = key
+        self.order = []  # To maintain the order of the cache items
 
     def put(self, key, item):
-        """ Assign to the dictionary """
-        if key and item:
-            if key in self.cache_data:
-                self.cache_data[key] = item
-            else:
-                self._push(key, item)
+        """
+        Add an item in the cache using FIFO policy.
+        If key or item is None, do nothing.
+        If cache exceeds MAX_ITEMS,
+        discard the first item added and print the key.
+        """
+        if key is None or item is None:
+            return
+
+        if key not in self.cache_data:
+            self.order.append(key)
+
+        self.cache_data[key] = item
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first_in_key = self.order.pop(0)
+            del self.cache_data[first_in_key]
+            print(f"DISCARD: {first_in_key}")
 
     def get(self, key):
-        """ Return the value linked """
-        if key is None or self.cache_data.get(key) is None:
-            return None
-        if key in self.cache_data:
-            value = self.cache_data[key]
-            return value
+        """
+        Get an item by key.
+        Return None if the key is None or doesn't exist.
+        """
+        return self.cache_data.get(key, None)

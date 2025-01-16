6c9e1a33dcdf00b5067f08730286323873a0cd07
Philippa Louise Giibwa <125802100+PhiLouGii@users.noreply.github.com>
2025-01-16 16:19:32 +0300

    done :)

--- caching/3-lru_cache.py
+++ caching/3-lru_cache.py
@@ -1,48 +1,60 @@
-#!/usr/bin/python3
-""" LRU Caching """
+#!/usr/bin/env python3
+"""
+LRU Cache Module
+"""
+
+
 from base_caching import BaseCaching
 
 
 class LRUCache(BaseCaching):
-    """ Class that inherits from BaseCaching and is a caching system """
+    """
+    LRU caching system where the
+    least recently used item is discarded
+    when the cache exceeds the limit.
+    """
 
     def __init__(self):
+        """
+        Initialize the cache system and track the order of usage.
+        """
         super().__init__()
-        self.head, self.tail = '-', '='
-        self.next, self.prev = {}, {}
-        self.handle(self.head, self.tail)
-
-    def handle(self, head, tail):
-        """ LRU algorithm, handle elements """
-        self.next[head], self.prev[tail] = tail, head
-
-    def _remove(self, key):
-        """ LRU algorithm, remove element """
-        self.handle(self.prev[key], self.next[key])
-        del self.prev[key], self.next[key], self.cache_data[key]
-
-    def _add(self, key, item):
-        """ LRU algorithm, add element """
-        self.cache_data[key] = item
-        self.handle(self.prev[self.tail], key)
-        self.handle(key, self.tail)
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            print("DISCARD: {}".format(self.next[self.head]))
-            self.remove(self.next[self.head])
+        self.usage_order = []  # To track the usage order of the cache items
 
     def put(self, key, item):
-        """ Assign to the directory """
-        if key and item:
-            if key in self.cache_data:
-                self._remove(key)
-            self._add(key, item)
+        """
+        Add an item to the cache using the LRU policy.
+        If key or item is None, do nothing.
+        If the cache exceeds MAX_ITEMS, discard
+        the least recently used item.
+        """
+        if key is None or item is None:
+            return
+
+        # If key already exists, remove it to update its usage order
+        if key in self.cache_data:
+            self.usage_order.remove(key)
+
+        self.cache_data[key] = item
+        self.usage_order.append(key)
+
+        # Check if the cache exceeds the limit
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            lru_key = self.usage_order.pop(0)
+            del self.cache_data[lru_key]
+            print(f"DISCARD: {lru_key}")
 
     def get(self, key):
-        """ Return the value linked """
-        if key is None or self.cache_data.get(key) is None:
+        """
+        Get an item by key.
+        Return None if the key is None or doesn't exist in cache_data.
+        If the key is found, update its usage order.
+        """
+        if key is None or key not in self.cache_data:
             return None
-        if key in self.cache_data:
-            value = self.cache_data[key]
-            self._remove(key)
-            self._add(key, value)
-            return value
+
+        # Update usage order by moving accessed key to the end
+        self.usage_order.remove(key)
+        self.usage_order.append(key)
+
+        return self.cache_data.get(key)
